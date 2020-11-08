efbecdcbd61a53aa699781c22d124b064daa0ee3
szendrattila <attila8000a@gmail.com>
2020-11-08 19:40:28 +0000

    added f6

---
+++ f6.py
@@ -0,0 +1,9 @@
+ora1 = "Lyukas ora"
+ora2 = "Angol"
+ora3 ="Munka"
+ora4 ="Matek"
+ora5 ="testnevelés"
+ora6 = "Irodalom"
+ora7 = "komplex"
+
+print('Hello....! \n +----------------+ \n I Hetfo \t \t  I \n +----------------+ \n I' ,ora1 ,'\t  I \n +----------------+ \n I' , ora2 , '\t \t  I \n +----------------+  \n I' , ora3 , '\t \t  I \n +----------------+ \n I' , ora4 , '\t \t  I \n +----------------+ \n I' , ora5, '\t  I \n +----------------+  \n I' , ora6 , '\t  I \n +----------------+ \n I' , ora7 , ' \t  I \n +----------------+'  ) 
